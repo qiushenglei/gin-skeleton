2d918eb05e257376465774bfb4dfc7f4258e5a0a
MrQ <mrq@mrq>
2023-10-28 00:06:07 +0800

    go generate

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import "github.com/qiushenglei/gin-skeleton/cmd"
 
+//go:generate go run ../gorm_gen/.
 //go:generate protoc --proto_path=../../proto/ --go_out=../../proto/  --go_opt=paths=source_relative  --go-grpc_out=../../proto/  --go-grpc_opt=paths=source_relative  orderstream.proto
 
 func main() {
