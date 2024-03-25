144914e1333f8fad8510a66cc67c4c6828faa9e2
MrQ <mrq@mrq>
2024-03-25 15:40:00 +0800

    grace

--- pkg/safe/gracestop.go
+++ pkg/safe/gracestop.go
@@ -3,14 +3,5 @@ package safe
 import "context"
 
 func GraceResidentProc(ctx context.Context, f func() error) {
-	go func() {
-		var isDone bool
-		go func() {
-			select {
-			case <-ctx.Done():
-				isDone = true
-			}
-		}
 
-	}
 }
