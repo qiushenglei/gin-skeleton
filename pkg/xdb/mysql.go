5f402d7fcd44baa894d5cf35585204640f0c5655
MrQ <mrq@mrq>
2024-03-27 14:57:58 +0800

    db init

---
+++ pkg/xdb/mysql.go
@@ -0,0 +1,64 @@
+package xdb
+
+import (
+	"fmt"
+	"github.com/qiushenglei/gin-skeleton/pkg/logs"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type RDB struct {
+	Username     string `json:"username"`
+	Password     string `json:"password"`
+	Host         string `json:"host"`
+	Port         int    `json:"port"`
+	DBName       string `json:"dbname"`
+	IdleConn     int    `json:"idle_conn"`
+	OpenConn     int    `json:"open_conn"`
+	LogWriteType int    `json:"log_write_type"`
+}
+
+const (
+	ConsoleLogWrite int = iota
+	FileLogWrite
+)
+
+func (r *RDB) RegisterMySQL() (*gorm.DB, error) {
+
+	var log logger.Interface
+	switch r.LogWriteType {
+	case FileLogWrite:
+		log = logs.NewGormLogger(
+			logs.Log,
+			logger.Config{
+				Colorful:             true,
+				LogLevel:             logger.Info,
+				ParameterizedQueries: false, // false展示value值，true只展示sql ?占位符
+			})
+	case ConsoleLogWrite:
+	default:
+		//log = logger.Default
+		log = logger.Default.LogMode(logger.Info)
+	}
+
+	// 建立 cancel_test Mysql 连接
+	var err error
+	DBConfig := &gorm.Config{
+
+		Logger: log,
+	}
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?timeout=5s&readTimeout=5s&writeTimeout=1s&parseTime=true&loc=Local&charset=utf8mb4,utf8", r.Username, r.Password, r.Host, r.Port, r.DBName)
+	cli, err := gorm.Open(mysql.Open(dsn), DBConfig)
+	if err != nil {
+		return cli, err
+	}
+
+	rawdb, err := cli.DB()
+	rawdb.SetMaxIdleConns(6)
+	rawdb.SetMaxOpenConns(6 * 2)
+	if err != nil {
+		return nil, err
+	}
+	return cli, nil
+}
---
+++ pkg/xdb/redis.go
@@ -0,0 +1,25 @@
+package xdb
+
+import (
+	"context"
+	"github.com/go-redis/redis/v8"
+)
+
+type RedisDB struct {
+	Address string `json:"address"`
+	DB      int    `json:"DB"`
+}
+
+func (r *RedisDB) RegisterRDBClient(c context.Context) (*redis.Client, error) {
+	opt := redis.Options{
+		Addr: r.Address,
+		DB:   r.DB,
+	}
+	cli := redis.NewClient(&opt)
+
+	if err := cli.Ping(c).Err(); err != nil {
+		return nil, err
+	}
+
+	return cli, nil
+}
